6edaf6c5d3f3c1f5d7fc6701a73f6a02c06db3c6
David <dabakovich@gmail.com>
2025-06-25 00:13:57 +0300

    feat: add prompt builder

--- src/rag_assistant/conversation.py
+++ src/rag_assistant/conversation.py
@@ -1,15 +1,23 @@
 import sys
 from llm import get_llm
 from langchain.schema import SystemMessage, HumanMessage, AIMessage
+from utils import load_yaml_config
+from paths import PROMPT_CONFIG_FPATH
+from prompt_builder import build_prompt_from_config
 
 
 class Conversation:
     def __init__(self, llm_name="gpt_nano"):
         self.llm = get_llm(llm_name)
+        self.prompts_config = load_yaml_config(PROMPT_CONFIG_FPATH)
 
     def run(self):
         messages = [
-            SystemMessage(content="You are a helpful AI assistant. Answer very shortly")
+            SystemMessage(
+                content=build_prompt_from_config(
+                    self.prompts_config["assistant_system_message"]
+                )
+            )
         ]
         while True:
             user_input = input("You: ")
--- src/rag_assistant/paths.py
+++ src/rag_assistant/paths.py
@@ -1,5 +1,8 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+RAG_ASSISTANT_DIR = os.path.join(ROOT_DIR, "rag_assistant")
 
 ENV_FPATH = os.path.join(ROOT_DIR, ".env")
+
+PROMPT_CONFIG_FPATH = os.path.join(RAG_ASSISTANT_DIR, "config", "prompt.yaml")
