a3aa62e1e019b40c5003ad6f8b0082f9533af4e0
David <dabakovich@gmail.com>
2025-06-26 17:17:36 +0300

    feat: integrate conversation memory and chain for enhanced dialogue management
    This update introduces ConversationBufferMemory and ConversationChain from the langchain library, allowing for persistent chat history and improved conversation flow. The run method has been modified to utilize these new components, enhancing the overall user experience.

--- src/core/conversation.py
+++ src/core/conversation.py
@@ -1,32 +1,41 @@
 import sys
 from llm.client import get_llm
+from langchain.memory import ConversationBufferMemory
+from langchain_community.chat_message_histories import FileChatMessageHistory
 from langchain.schema import SystemMessage, HumanMessage, AIMessage
 from common.utils import load_yaml_config
 from paths import PROMPT_CONFIG_FPATH
 from llm.prompt_builder import build_prompt_from_config
+from langchain.chains.conversation.base import ConversationChain
 
 
 class Conversation:
     def __init__(self, llm_name="gpt_nano"):
         self.llm = get_llm(llm_name)
         self.prompts_config = load_yaml_config(PROMPT_CONFIG_FPATH)
+        self.memory = ConversationBufferMemory(
+            chat_memory=FileChatMessageHistory("chats/chat_history.json"),
+            return_messages=True,
+        )
+        self.conversation = ConversationChain(llm=self.llm, memory=self.memory)
 
     def run(self):
-        messages = [
+        self.memory.chat_memory.add_message(
             SystemMessage(
                 content=build_prompt_from_config(
                     self.prompts_config["assistant_system_message"]
                 )
             )
-        ]
+        )
         while True:
             user_input = input("You: ")
 
             if user_input.lower() == "q":
                 break
 
-            messages.append(HumanMessage(content=user_input))
-            stream_generator = self.llm.stream(messages)
+            self.memory.chat_memory.add_message(HumanMessage(content=user_input))
+
+            stream_generator = self.llm.stream(self.memory.chat_memory.messages)
 
             response = ""
             sys.stdout.write("AI: ")
@@ -37,4 +46,4 @@ class Conversation:
 
             sys.stdout.write("\n")
 
-            messages.append(AIMessage(content=response))
+            self.memory.chat_memory.add_message(AIMessage(content=response))
